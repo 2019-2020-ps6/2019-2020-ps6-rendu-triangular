a09322f21f239e22f93251f5e45b46a0ce6da110
Asaréel <asareel56@gmail.com>
2020-03-23 17:11:06 +0100

    App Update following the new components

--- frond-end/src/app/app.module.ts
+++ frond-end/src/app/app.module.ts
@@ -1,12 +1,24 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
+import * as $ from 'jquery'
+
 
 import {AppComponent} from './app.component';
 import {QuizListComponent} from './quizzes/quiz-list/quiz-list.component';
 import {QuizComponent} from './quizzes/quiz/quiz.component';
 import {HeaderComponent} from './header/header.component';
 import {QuizFormComponent} from './quizzes/quiz-form/quiz-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import { FooterComponent } from './footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +27,20 @@ import {QuizFormComponent} from './quizzes/quiz-form/quiz-form.component';
     QuizComponent,
     HeaderComponent,
     QuizFormComponent,
+    FooterComponent,
+    MainContentComponent,
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
