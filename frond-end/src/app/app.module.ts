d6e97764475471b597dcffc9e341a017bfb31d0e
Asaréel <asareel56@gmail.com>
2020-03-24 15:32:24 +0100

    Update app-component

--- frond-end/src/app/app.module.ts
+++ frond-end/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
-import * as $ from 'jquery'
 
 
 import {AppComponent} from './app.component';
